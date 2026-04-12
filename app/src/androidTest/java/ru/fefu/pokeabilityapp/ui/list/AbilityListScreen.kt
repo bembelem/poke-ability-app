5d7ea2fb2f7eac7659d924ba7ff42476aa749d04
bembelem <nikvedd@gmail.com>
2026-04-12 12:10:54 +1000

    feat: add unit, dao and ui tests

--- app/src/androidTest/java/ru/fefu/pokeabilityapp/ui/list/AbilityListScreen.kt
+++ app/src/androidTest/java/ru/fefu/pokeabilityapp/ui/list/AbilityListScreen.kt
@@ -1,4 +1,54 @@
 package ru.fefu.pokeabilityapp.ui.list
 
-class AbilityListScreen {
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import org.junit.Rule
+import org.junit.Test
+import ru.fefu.pokeabilityapp.domain.model.AbilityFilter
+import ru.fefu.pokeabilityapp.domain.model.AbilityItem
+
+class AbilityListScreenTest {
+
+    @get:Rule
+    val composeRule = createComposeRule()
+
+    private val overgrow = AbilityItem(id = 1, name = "overgrow")
+    private val blaze = AbilityItem(id = 2, name = "blaze")
+
+    @Test
+    fun abilityName_isShown_whenListIsNotEmpty() {
+        composeRule.setContent {
+            AbilityList(
+                abilities = listOf(overgrow, blaze),
+                favourites = emptySet(),
+                onAbilityClick = {},
+                onToggleFavourite = {},
+                onLoadMore = {},
+                isLoadingMore = false,
+                canLoadMore = false
+            )
+        }
+
+        composeRule.onNodeWithText("Overgrow").assertIsDisplayed()
+        composeRule.onNodeWithText("Blaze").assertIsDisplayed()
+    }
+
+    @Test
+    fun emptyFavourites_showsHintText() {
+        composeRule.setContent {
+            AbilityListScreen(
+                state = AbilityListUiState(filter = AbilityFilter.FAVOURITES),
+                visibleAbilities = emptyList(),
+                onAbilityClick = {},
+                onFilterChange = {},
+                onToggleFavourite = {},
+                onLoadMore = {},
+                onRetry = {},
+            )
+        }
+
+        composeRule.onNodeWithText("No favourites yet\nSwipe to add favourite")
+            .assertIsDisplayed()
+    }
 }
